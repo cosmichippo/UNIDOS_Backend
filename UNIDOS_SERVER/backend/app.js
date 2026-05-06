076315f10733e5d929a045619d9890b41c05813e
Juan-Fernando <juprmora@ucsc.edu>
2026-05-06 12:05:52 -0700

    updated to grab from airgradient

--- UNIDOS_SERVER/backend/app.js
+++ UNIDOS_SERVER/backend/app.js
@@ -10,21 +10,36 @@ app.use(cors()); // Allow frontend access
 const PORT = process.env.PORT || 3000;
 
 // Route to get air quality by city
-app.get('/air-quality', async (req, res) => {
+app.get('/air-quality/:id', async (req, res) => {
+    //Kman Ids
+    ids = ["183709", "183644", "171892", "171872"]
     try {
-        const city = req.params.city;
+        const locationId = req.params.id;
         // this has to be better
+        if (!(ids.includes(locationId))){
+            console.log("doesn't include id")
+            throw 500;
+        }
         const apiKey = process.env.AQI_API_KEY;
-        
-        // Example using WAQI API structure
-        const response = await axios.get(`${process.env.AQI_BASE_URL}${city}/?token=${apiKey}`);
-        
-        // Return the data to your frontend
-        res.json(response.data);
+        const headers = {token: apiKey};
+        const response = await axios.get(`https://api.airgradient.com/public/locations/${locationId}/measures/current`, 
+           { headers }
+        );
+        if (response.headers == 200) {
+            res.json(response.data)
+        } else {
+            res.status(response.headers).send();
+            console.log(response);
+        }
+        //res.json(response.data);
     } catch (error) {
-        res.status(500).json({ error: 'Failed to fetch air quality data' });
-    }
+        console.log(error);
+        res.status(500).send();
+    } 
+    // i should read up on express responses, chaning responses, good practices for code. 
+    // I don't like that there are multiple locations where this code can terminate from.
+    // I would like this to be better organized to be expandable.
 });
 
-module.exports(app);
+module.exports = {app};
 
